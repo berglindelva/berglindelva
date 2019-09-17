79c28008ea64b01db586af748c291085533b5634
Berglind Sigvaldadóttir <berglindsigvaldadottir@Berglinds-MacBook-2.local>
2019-09-17 10:32:19 +0000

    komment

---
+++ tile_traveller/tile_traveller.py
@@ -0,0 +1,9 @@
+#The player starts in square (1,1)
+#We will start by assigning x and y its first coordinates
+#We need four functions to define when we can move from one square to another
+#We need to make a function to define the possibilites the user could take and we use strings
+#We make a function that asks the user for an input on which direction he chooses
+#We make a function that returns 1 if true and 0 if false
+#We make a function to define the value of x and y that corresponds to the movements
+#Now we have made all the functions required and start on the main code
+#The main code starts with a while loop and continous until the user reaches the victory square
